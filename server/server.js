233d08f2a9c670bac877be2bf6cea015e65188da
fabs <fabio.gianini@gmx.ch>
2012-06-20 20:12:08 +0200

    socket server up to date

--- server/server.js
+++ server/server.js
@@ -1,11 +1,49 @@
-var io = require('socket.io').listen(18815);
+function NetworkEngine()
+{
+    this.connection=null;
+    this.port=process.env.PORT;
+    console.log(this.port);
+}
+NetworkEngine.prototype.connect=function()
+{
+    this.connection=require("socket.io").listen(27069);
+}
+NetworkEngine.prototype.load=function()
+{
+    this.connection.on('connection', 
+        function(socket)
+        {
+            socket.emit('CONNECTION', {state:true});
+            
+            socket.on('ECHO', 
+                function(data)
+                {
+                    socket.emit('ECHO', data);
+                
+                }
+            );
+            socket.on('update',
+                function(data)
+                {
+                    socket.broadcast.emit('update', data);
+                }
+            );
+        }
+    );
+    
+}
 
-var TEST_CLIENT="TEST_CLIENT";
-var TEST_SERVER="TEST_SERVER";
+function DataBase(name)
+{
+    this.data=new Array();
+    this.name=name;
+}
+DataBase.prototype.store=function(key, data)
+{
+    this.data[key]=data;
+}
 
-io.sockets.on('connection', function (socket) {
-  socket.on(TEST_CLIENT, function (data) {
-    console.log("incomming request");
-    socket.emit(TEST_SERVER, {text:TEST_SERVER});
-  });
-});
+
+var network=new NetworkEngine();
+network.connect();
+network.load();
